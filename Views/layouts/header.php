ae61b9174d8517976a6df34a0df0130d7b49752c
steveclifton <steve.clifton@outlook.com>
2016-11-14 20:13:38 +1300

    Add header to the app

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -1,37 +1,27 @@
-
-
-<div class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
+<div class="navbar navbar-default">
+    <div class="container-fluid">
         <div class="navbar-header">
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/" style="background-color: lightgrey; font-family: Calibri">Massey Marking App</a>
+            <a class="navbar-brand" href="/" style="background-color: lightgrey; font-family: Calibri">Massey Marking
+                App</a>
         </div>
-            <div class="navbar-collapse collapse" id="navbar-main">
-                <ul class="nav navbar-nav">
+        <div class="navbar-collapse collapse" id="navbar-main">
+            <ul class="nav navbar-nav navbar-right">
+                <?php
 
-                    <?php
-                        if (isset($_SESSION['ID'])) {
-                            echo "
-                                <li><a href=\"#\">Results</a></li>
-                                <li><a href=\"#\">Contact</a></li>
-                            ";
-                        }
-                    ?>
-                </ul>
-                <form method="" action="#" class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="student_id" placeholder="Student ID" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="Password" required>
-                    </div>
-                    <button type="submit" class="btn btn-default">Sign In</button>
-                </form>
-            </div>
+                    if (isset($_SESSION['id'])) {
+                        $studentName = $_SESSION['first_name'] . " " . $_SESSION['last_name'];
+                        echo "
+                              <li><a>$studentName</a></li>
+                              <li><a href='logout'>Logout</a></li>
+                              ";
+                    }
+                ?>
+            </ul>
+        </div>
     </div>
 </div>
-
