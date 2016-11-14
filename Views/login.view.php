3da5da6dc7cdd7379d4f336cb4aa27fc01ac4e6c
steveclifton <steve.clifton@outlook.com>
2016-11-14 20:13:58 +1300

    Add login view

--- Views/login.view.php
+++ Views/login.view.php
@@ -1,5 +1,40 @@
+<link rel="stylesheet" type="text/css" href="../Views/css/login.css">
 
-    <br><br>
-    <h1>Hello</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-login">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <form action="login" method="post" style="display: block;">
+                                <h2>LOGIN</h2>
+                                <div class="form-group">
+                                    <input type="text" name="student_id" id="student_id" tabindex="1" class="form-control"
+                                           placeholder="Student ID" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2"
+                                           class="form-control" placeholder="Password">
+                                </div>
+                                <div class="col-xs-6 form-group pull-left checkbox">
+                                    <input id="checkbox1" type="checkbox" name="remember">
+                                    <label for="checkbox1">Remember Me</label>
+                                </div>
+                                <div class="col-xs-6 form-group pull-right">
+                                    <input type="submit" name="login-submit" id="login-submit" tabindex="4"
+                                           class="form-control btn btn-login" value="Log In">
+                                </div>
+                            </form>
+                            <form action="register" method="post">
+                                <input type="submit">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
