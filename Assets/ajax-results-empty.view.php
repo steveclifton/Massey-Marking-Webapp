5eb1fc1e66ac465b163ef9cc830b7ee0a97336f6
steveclifton <steve.clifton@outlook.com>
2016-11-16 22:07:41 +1300

    Remove unused pages

--- Assets/ajax-results-empty.view.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td><p class="listdata">SKU</p></td>
-    <td><p class="listdata">Product Name</p></td>
-    <td><p class="listdata">Category</p></td>
-    <td><p class="listdata">Price</p></td>
-    <td><p class="listdata">Qty</p></td>
-</tr>
