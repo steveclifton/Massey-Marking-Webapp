e2d96d79ca7989eafb236ba28ec0b1b347572e1f
steveclifton <steve.clifton@outlook.com>
2016-11-17 08:03:28 +1300

    Add class for jQ

--- Views/login.view.php
+++ Views/login.view.php
@@ -7,19 +7,16 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form action="login" method="post" style="display: block;">
+                            <form class="validatedForm" action="login" method="post" style="display: block;">
                                 <h2>LOGIN</h2>
                                 <div class="form-group">
-                                    <input type="text" name="student_id" id="student_id" tabindex="1" class="form-control"
+                                    <input type="text" name="student_id" id="student_id" tabindex="1"
+                                           class="form-control"
                                            placeholder="Student ID" value="" required>
                                 </div>
                                 <div class="form-group">
                                     <input type="password" name="password" id="password" tabindex="2"
                                            class="form-control" placeholder="Password" required>
-                                </div>
-                                <div class="col-xs-6 form-group pull-left checkbox">
-                                    <input id="checkbox1" type="checkbox" name="remember">
-                                    <label for="checkbox1">Remember Me</label>
                                 </div>
                                 <div class="col-xs-6 form-group pull-right">
                                     <input type="submit" name="login-submit" id="login-submit" tabindex="4"
