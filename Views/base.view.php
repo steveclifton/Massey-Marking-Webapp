802cbecaba795a83b7e7d986458dad2626b6000c
steveclifton <steve.clifton@outlook.com>
2016-11-16 22:02:37 +1300

    Refactor base view

--- Views/base.view.php
+++ Views/base.view.php
@@ -3,7 +3,7 @@
     <head>
         <title>Massey Marking App - <?= $pageTitle; ?></title>
         <meta charset="utf-8">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <script src="/Assets/AjaxVerify.js"></script>
 
@@ -12,9 +12,9 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
     </head>
     <body>
+
         <div class="container">
             <?php include('/var/www/marking/Views/layouts/header.php'); ?>
         </div>
