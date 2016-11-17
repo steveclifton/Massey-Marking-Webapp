d3c6f5333e9d8e0544fd67d626d196821a526acf
steveclifton <steve.clifton@outlook.com>
2016-11-17 21:28:59 +1300

    Refactor code

--- Views/layouts/leftside.php
+++ Views/layouts/leftside.php
@@ -6,23 +6,20 @@
  *
  * jQuery used on the Class = assignment, and uses the ID to determine which
  *   was clicked
- *
- *
  */
 
 
 if (isset($_SESSION['id'])) {
     echo "
             <div class=\"col-xs-6 col-sm-2 sidebar-offcanvas\" id=\"sidebar\" role=\"navigation\" style=\"padding-top: 10px\">
-
-            <ul class=\"nav\" style=\"padding-top: 30px\">
-            
-            <h4 style='padding-left: 12px; font-size: 20pt;'>Assignments</h4>";
+            <h4 style='padding-left: 12px; font-size: 20pt;'>Assignments</h4>
+            <ul class=\"nav\">
+          ";
 
     $weeks = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
     foreach ($weeks as $week) {
-        print "<li><a class=\"assignment\" id=\"$week\" style='text-decoration: underline; font-size: 12pt;'>Assignment $week</a></li>";
+        echo "<li><a class=\"assignment\" id=\"$week\" style='text-decoration: underline; font-size: 12pt;'>Assignment $week</a></li>";
     }
 }
 echo "</ul></div>";
