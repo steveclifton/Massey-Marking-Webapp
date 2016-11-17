c452446faa5d2287a0d16ef6b1b61cf58709ea49
steveclifton <steve.clifton@outlook.com>
2016-11-17 22:29:13 +1300

    Add form checking of file being uploaded

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -1,13 +1,14 @@
 <div class="col-xs-2 col-sm-10" id="centerdisplay" style="padding-left: 140px; padding-top: 30px;">
 
-    <form action="#" method="post">
+    <form onsubmit="return checkFileType(this)" action="/upload" method="post" enctype="multipart/form-data">
         <h4>Select Assignment .CPP file to upload</h4><br>
-        <input type="file" id="myFile"><br><br>
-        <input type="button" class="btn btn-primary" value="Submit">
+        <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+        <input type="submit" id="submitButtom" class="btn btn-primary" value="submit">
     </form><br>
 
     <h4 style="padding-top: 40px">Output Results</h4>
-    <p>SOME THING HERE IS THWEWRWERFWIEFR WNRGJN
+
+    <p id="">SOME THING HERE IS THWEWRWERFWIEFR WNRGJN
         ERJGN WEJNG WJNF WEJN JWEGN SOME THING HERE IS
         THWEWRWERFWIEFR WNRGJN ERJGN WEJNG WJNF WEJN JWEGN
         SOME THING HERE IS THWEWRWERFWIEFR WNRGJN ERJGN WEJNG
