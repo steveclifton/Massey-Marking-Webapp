d409a56d80eab61ae5b7d85ec06fd1d47d18ef1e
steveclifton <steve.clifton@outlook.com>
2016-11-17 20:33:07 +1300

    Add the ability to handle assignment selections

--- Views/js/verify.js
+++ Views/js/verify.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
 
-    $('#login-submit').click(function(){
-        alert("Login-Submit Alert HERE");
+    // $('#login-submit').click(function(){
+    //     alert("Login-Submit Alert HERE");
+    // });
+
+    /**
+     * This function executes when the Assignment number is clicked
+     *
+     * Gets the ID from the Class to be used to query the Database
+     */
+    $('.assignment').on('click', function () {
+        var id = $(this).attr('id');
+        alert(id);
+
     });
 
 });
