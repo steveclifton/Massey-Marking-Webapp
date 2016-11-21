db4cfd3122f5d974818ae0b8b64163d67a5711f5
steveclifton <steve.clifton@outlook.com>
2016-11-21 19:50:52 +1300

    Add assignment linking

--- Views/layouts/leftside.php
+++ Views/layouts/leftside.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['id'])) {
     $weeks = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
     foreach ($weeks as $week) {
-        echo "<li><a class=\"assignment\" id=\"$week\" style='text-decoration: underline; font-size: 12pt;'>Assignment $week</a></li>";
+        echo "<li><a class=\"assignment\" href=\"/assignment?num=$week\" id=\"$week\" style='text-decoration: underline; font-size: 12pt;'>Assignment $week</a></li>";
     }
 }
 echo "</ul></div>";
