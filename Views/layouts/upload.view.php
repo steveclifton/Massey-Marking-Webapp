e0281018c2e0d7432c9c05d7114bd45bbe4632c0
steveclifton <steve.clifton@outlook.com>
2016-11-21 19:49:54 +1300

    Add upload form view

---
+++ Views/layouts/upload.view.php
@@ -0,0 +1,10 @@
+<div class="col-xs-2 col-sm-10" id="centerdisplay" style="padding-left: 140px; padding-top: 30px;">
+
+    <form onsubmit="return checkFileType(this)" action="/upload" method="post" enctype="multipart/form-data">
+        <h4>Select Assignment .CPP file to upload</h4><br>
+        <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+        <input type="submit" id="submitButtom" class="btn btn-primary" value="submit">
+    </form>
+    <br>
+
+</div>
