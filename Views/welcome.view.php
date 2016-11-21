bbb5a70f387eaf7907435da19f0c90b19d3a53da
steveclifton <steve.clifton@outlook.com>
2016-11-21 19:49:29 +1300

    Remove form from welcome page

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -1,31 +1,9 @@
-<div class="col-xs-2 col-sm-10" id="centerdisplay" style="padding-left: 140px; padding-top: 30px;">
-
-    <form onsubmit="return checkFileType(this)" action="/upload" method="post" enctype="multipart/form-data">
-        <h4>Select Assignment .CPP file to upload</h4><br>
-        <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
-        <input type="submit" id="submitButtom" class="btn btn-primary" value="submit">
-    </form><br>
-
     <h4 style="padding-top: 40px">Output Results</h4>
 
-    <p id="">SOME THING HERE IS THWEWRWERFWIEFR WNRGJN
-        ERJGN WEJNG WJNF WEJN JWEGN SOME THING HERE IS
-        THWEWRWERFWIEFR WNRGJN ERJGN WEJNG WJNF WEJN JWEGN
-        SOME THING HERE IS THWEWRWERFWIEFR WNRGJN ERJGN WEJNG
-        WJNF WEJN JWEGN SOME THING HERE IS THWEWRWERFWIEFR WNRGJN
-        ERJGN WEJNG WJNF WEJN JWEGN SOME THING HERE IS
-        THWEWRWERFWIEFR WNRGJN ERJGN WEJNG WJNF WEJN JWEGN
-        SOME THING HERE IS THWEWRWERFWIEFR WNRGJN ERJGN
-        WEJNG WJNF WEJN JWEGN SOME THING HERE IS THWEWRWERFWIEFR
-        WNRGJN ERJGN WEJNG WJNF WEJN JWEGN SOME THING HERE IS
-        THWEWRWERFWIEFR WNRGJN ERJGN WEJNG WJNF WEJN JWEGN SOME
-        THING HERE IS THWEWRWERFWIEFR WNRGJN ERJGN WEJNG WJNF
-        WEJN JWEGN SOME THING HERE IS THWEWRWERFWIEFR WNRGJN
-        ERJGN WEJNG WJNF WEJN JWEGN SOME THING HERE IS
-        THWEWRWERFWIEFR WNRGJN ERJGN WEJNG WJNF WEJN JWEGN
-        SOME THING HERE IS THWEWRWERFWIEFR WNRGJN ERJGN WEJNG
-        WJNF WEJN JWEGN SOME THING HERE IS THWEWRWERFWIEFR WNRGJN
-        ERJGN WEJNG WJNF WEJN JWEGN
+    <p id="">
+        This is where the students overall marks will be displayed <br>
+        Assignment 1 - 10/10<br>
+        Assignment 2 - 9/10<br>
     </p>
 
 </div>
