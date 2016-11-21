6ab243cbb2bfe1e99faa63a48d415e19e08bcc28
steveclifton <steve.clifton@outlook.com>
2016-11-21 19:51:13 +1300

    Add Assignment Controller and multiple functions

---
+++ Controllers/Assignment.Controller.php
@@ -0,0 +1,96 @@
+<?php
+
+
+namespace Marking\Controllers;
+
+use Exception;
+
+class Assignment extends Base
+{
+
+    /**
+     * Gets the users details from SESSION and passes to the view
+     */
+    public function loadAssignmentView()
+    {
+        $this->isNotLoggedIn();
+
+        $viewData = "";
+
+        $this->render('Assignment', 'assignment' . $_GET['num'] . '.view', $viewData);
+    }
+
+    public function uploadFile()
+    {
+
+        // Gets the assignment number from the view
+        $assNum = $this->getAssignmentNumber($_SERVER['HTTP_REFERER']);
+
+        // Sets the target directory
+        $target_dir = "/var/www/marking/uploads/" . $_SESSION['student_id'] . "/" . $assNum;
+
+
+        /**
+         * If the target directory does not exist, CREATE it
+         *  - otherwise, remove all files from it
+         */
+        if (!is_dir($target_dir)) {
+            mkdir($target_dir, 0700);
+        } else {
+            $this->removeAllFiles($target_dir);
+        }
+
+        // Sets the target file directory and name
+        $target_file = $target_dir . "/A$assNum.cpp";
+
+
+        try {
+            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+        } catch (Exception $e) {
+            var_dump($e); //TODO remove
+            die();
+        }
+
+        if ($this->compileAssignment($target_dir)) {
+            // This means the assignment compiled
+        }
+
+
+        header("Location: {$_SERVER["HTTP_REFERER"]}");
+    }
+
+
+    private function markAssignment()
+    {
+
+    }
+
+    private function compileAssignment($target_dir)
+    {
+        // todo
+
+
+        return true;
+    }
+
+    private function getAssignmentNumber($httpRef)
+    {
+        $number = explode('=', $httpRef);
+        return $number[1];
+    }
+
+    private function removeAllFiles($target_dir)
+    {
+        $files = glob($target_dir . "/*"); // get all file names
+
+        // Removes each file in the directory
+        foreach ($files as $file){
+            if (is_file($file))
+                unlink($file); // delete file
+        }
+        return true;
+    }
+
+
+}
+
