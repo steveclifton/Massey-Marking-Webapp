694d95b4e4af0665e01745f6b49060ebc9b75aec
steveclifton <steve.clifton@outlook.com>
2016-11-22 13:24:06 +1300

    Add admin side panel

---
+++ Views/layouts/adminpanel.view.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * If the user is logged in (SessionID is set) then a list of available
+ *   assignments will be displayed.
+ *
+ * jQuery used on the Class = assignment, and uses the ID to determine which
+ *   was clicked
+ */
+
+
+if (isset($_SESSION['id'])) {
+    echo "
+            <div class=\"col-xs-6 col-sm-2 sidebar-offcanvas\" id=\"sidebar\" role=\"navigation\" style=\"padding-top: 10px\">
+            <h4 style='padding-left: 12px; font-size: 18pt;'>Admin Panel</h4>
+            <ul class=\"nav\">
+          ";
+
+    $weeks = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+    foreach ($weeks as $week) {
+        echo "<li><a class=\"assignment\" href=\"/assignment?num=$week\" id=\"$week\" style='text-decoration: underline; font-size: 12pt;'>Stuff $week</a></li>";
+    }
+}
+echo "</ul></div>";
+
+
+
+
