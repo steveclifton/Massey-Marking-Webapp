312cde885d76d451f4f8b8d81ff39a8951b35214
steveclifton <steve.clifton@outlook.com>
2016-11-22 13:51:42 +1300

    Remove class from DIV

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -1,5 +1,5 @@
-<div class="col-xs-2 col-sm-10" id="centerdisplay" style="padding-left: 140px; padding-top: 30px;">
+<div id="centerdisplay" style="padding-left: 100px; padding-top: 30px;">
 
-    <h1>here</h1>
+    <h1>Admin Stuff</h1>
 
 </div>
