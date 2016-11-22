006c103143d83b26dd3ebcba3e5e4ca476694848
steveclifton <steve.clifton@outlook.com>
2016-11-22 13:21:05 +1300

    Refactor unused code

--- Models/User.Model.php
+++ Models/User.Model.php
@@ -2,8 +2,6 @@
 
 namespace Marking\Models;
 
-use Marking\Exceptions\CustomException;
-
 use PDO;
 
 /**
@@ -30,60 +28,7 @@ class User extends Base
     }
 
     /**
-     * Creates a new user if registration information is valid
-     *
-     * @param $userData
-     * @return User - returns a new user object - used to log the user in automatically
-     */
-//    public function create($userData)
-//    {
-//
-//        /* Sanitizes and filters data before being inserted */
-//        $data['student_id'] = $this->database->real_escape_string($userData['username']);
-//        $data['first_name'] = ucfirst($this->database->real_escape_string($userData['first_name']));
-//        $data['last_name'] = ucfirst($this->database->real_escape_string($userData['last_name']));
-//        $data['password'] = $this->database->real_escape_string($userData['password']);
-//        $data['email'] = strtolower($this->database->real_escape_string($userData['email']));
-//
-//        $data['email'] = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
-//
-//        $password = password_hash($data['password'], PASSWORD_DEFAULT);
-//
-//        $this->database->query("INSERT INTO `users` (
-//                                            `id`,
-//                                            `student_id`,
-//                                            `first_name`,
-//                                            `last_name`,
-//                                            `password`,
-//                                            `email`,
-//                                            `create_date`
-//                                            ) VALUES (
-//                                            NULL,
-//                                             '".$data['student_id']."',
-//                                             '".$data['first_name']."',
-//                                             '".$data['last_name']."',
-//                                             '".$password."',
-//                                             '".$data['email']."',
-//                                             CURRENT_TIMESTAMP
-//                                             )"
-//        );
-//
-//        $newUserData = $this->getUserByStudentId($data['student_id']);
-//
-//        $newUser = new User();
-//
-//        foreach ($newUserData as $key => $value) {
-//            $newUser->$key = $value;
-//        }
-//
-//        return $newUser;
-//    }
-
-    /**
      * Verifies whether the users student id and password exist and match
-     *
-     * @param $userData
-     * @return mixed
      */
     public function verify($userData)
     {
@@ -100,6 +45,7 @@ class User extends Base
             $_SESSION['failedLogin'] = "Please check login details";
         }
     }
+
 
 
 }
