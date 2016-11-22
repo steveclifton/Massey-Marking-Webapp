6a54cdbf584cccf9fba91082dfa33c56414c2bf7
steveclifton <steve.clifton@outlook.com>
2016-11-22 13:36:45 +1300

    Refactor Admin view

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -1,5 +1,7 @@
+<?php
+    include('/var/www/marking/Views/layouts/adminpanel.view.php');
+?>
+
 <div class="col-xs-2 col-sm-10" id="centerdisplay" style="padding-left: 140px; padding-top: 30px;">
-
-    <h1>here</h1>
-
+    <h1>Admin Login</h1>
 </div>
