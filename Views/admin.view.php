1bd6b16657876499d87fba21d0bd2f21ffcbbc94
steveclifton <steve.clifton@outlook.com>
2016-11-23 14:28:22 +1300

    Removed panel from admin main view

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -1,13 +1,3 @@
-<div class="col-md-3" id="sidebar" role="navigation" style="padding-top: 20px">
-    <?php
-    if (isset($_SESSION['user_type'])) {
-        if ($_SESSION['user_type'] == 'admin') {
-            include('/var/www/marking/Views/layouts/adminpanel.view.php');
-        }
-    }
-    ?>
-</div>
-
 
 <div id="centerdisplay" style="padding-left: 100px; padding-top: 30px;">
 
