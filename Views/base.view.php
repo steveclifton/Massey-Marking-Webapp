73876d8e97f1ebf402a184008761a2774031fc6f
steveclifton <steve.clifton@outlook.com>
2016-11-23 14:30:04 +1300

    Refactor base view

--- Views/base.view.php
+++ Views/base.view.php
@@ -1,43 +1,46 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Massey Marking System - <?= $pageTitle; ?></title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<head>
+    <title>Massey Marking System - <?= $pageTitle; ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
-        <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src="../Views/js/verify.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.js"
+            integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
+    <script src="../Views/js/verify.js"></script>
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    </head>
-    <body>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</head>
+<body>
 
-        <div class="container-fluid">
-            <?php include('/var/www/marking/Views/layouts/header.php'); ?>
-        </div>
+<div class="container-fluid">
+    <?php include('/var/www/marking/Views/layouts/header.php'); ?>
+</div>
 
-        <div class="page-container">
-            <div class="container-fluid">
-                <div class="row">
+<div class="page-container">
+    <div class="container-fluid">
+        <div class="row">
 
+            <?php
+            include('/var/www/marking/Views/layouts/sidepanels/sidepanel.view.php')
+            ?>
 
-
-                    <div class="col-md-9" style="padding-top: 20px">
-                        <?php include($viewName . '.php'); ?>
-                    </div>
-
-                </div>
+            <div class="col-md-9" style="padding-top: 20px">
+                <?php include($viewName . '.php'); ?>
             </div>
+
         </div>
-
-
-
-
+    </div>
+</div>
 
 
 <!--        --><?php //include('/var/www/marking/Views/layouts/footer.php')?>
 
-    </body>
+</body>
 </html>
