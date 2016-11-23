9e8c66652bfb40b86d7dc6cfda93ecc62770bd2f
steveclifton <steve.clifton@outlook.com>
2016-11-23 14:28:36 +1300

    Remove panel from main welcome view

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -1,15 +1,3 @@
-<div class="col-md-3" id="sidebar" role="navigation" style="padding-top: 20px">
-    <?php
-    if (isset($_SESSION['user_type'])) {
-        if ($_SESSION['user_type'] == 'student') {
-            include('/var/www/marking/Views/layouts/studentpanel.view.php');
-        }
-    }
-    ?>
-</div>
-
-
-
 <div id="centerdisplay" >
 
     <h1>Student Stuff</h1>
