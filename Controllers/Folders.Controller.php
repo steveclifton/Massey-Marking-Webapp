2415a71e9e2b6857c966f7877065568aaa8de48d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-04 22:39:24 +1300

    Copies the test files into the students folder

--- Controllers/Folders.Controller.php
+++ Controllers/Folders.Controller.php
@@ -8,49 +8,41 @@ namespace Marking\Controllers;
 class Folders extends Base
 {
 
+
     public function createFolders($studentId)
     {
+        /**
+         * Gets the number of Assignments specified
+         */
+        $assignmentNumber = new AssignmentConfig();
+        $assignmentNumber = $assignmentNumber->getAssignmentNumber();
+
+
         /**
          * If the directory does not exist for a student, create it
          *  - once created
          */
-        if (!is_dir("/home/student/" . $studentId)) {
-            system("sudo mkdir /home/student/" . $studentId);
-            system("sudo chmod 777 /home/student/" . $studentId);
+        if (!is_dir("/home/student/$studentId")) {
+            system("sudo mkdir /home/student/$studentId");
+            system("sudo chmod 777 /home/student/$studentId");
         } else {
             /* The folder (and student) already exists so all files are deleted out of its account */
-            for ($i = 1; $i <= 8; $i++) {
-                $this->removeAllFiles("/home/student" . $studentId . "/A" . $i); // Now is in the assignment folder
+            for ($i = 1; $i <= $assignmentNumber; $i++) {
+                system("sudo rm /home/student/$studentId/A$i/*");
             }
         }
 
         /**
          * If the directory exists, create the assignment folder inside
+         *  - Creates new folders
+         *  - Copies assignment
          */
         if (is_dir("/home/student/" . $studentId)) {
-            for ($i = 1; $i <= 8; $i++) {
-                system("sudo mkdir /home/student/" . $studentId . "/A" . $i);
-                system("sudo chmod 777 /home/student/" . $studentId . "/A" . $i);
+            for ($i = 1; $i <= $assignmentNumber; $i++) {
+                system("sudo mkdir /home/student/$studentId/A$i");
+                system("sudo chmod 777 /home/student/$studentId/A$i");
+                system("sudo cp /home/assignmentfiles/A$i/*" . " /home/student/$studentId/A$i");
             }
         }
     }
-
-
-
-    /**
-     * Removes all the files in a directory
-     */
-    private function removeAllFiles($target_dir)
-    {
-        $files = glob($target_dir . "/*"); // get all file names
-
-        // Removes each file in the directory
-        foreach ($files as $file){
-            if (is_file($file))
-                unlink($file); // delete file
-        }
-        return true;
-    }
-
-
 }
