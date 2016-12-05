0cddb1d0c8ebdd518a90587115b0538fdda8b183
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-05 16:29:48 +1300

    Add method to retreive the students assignment mark

--- Models/Marks.Model.php
+++ Models/Marks.Model.php
@@ -3,7 +3,8 @@
 namespace Marking\Models;
 
 use Marking\Exceptions\CustomException;
-
+use PDO;
+use PDOException;
 
 /**
  * Class Marks
@@ -12,7 +13,23 @@ use Marking\Exceptions\CustomException;
 class Marks extends Base
 {
 
+    public function getUsersMarks($studentId, $assignmentNumber, $semester)
+    {
+        $sql = "SELECT mark 
+                FROM `marks` 
+                WHERE marks.student_id = '$studentId' 
+                AND marks.semester = '$semester' 
+                AND marks.assignment_number = '$assignmentNumber'
+                ";
 
+        $stm = $this->database->prepare(($sql), array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+        $stm->execute(array('$studentId, $assignmentNumber, $semester'));
+
+        $data = $stm->fetchAll();
+
+        return $data;
+    }
 
 
 
