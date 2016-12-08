5083dd6444918c2fe44dc330bac26cfe6a8d4cef
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-08 17:41:07 +1300

    Refactor side panel

--- Views/layouts/sidepanels/sidepanel.view.php
+++ Views/layouts/sidepanels/sidepanel.view.php
@@ -1,6 +1,6 @@
 <?php
     if (isset($_SESSION['user_type'])) {
-        echo "<div class=\"col-md-3\" id=\"sidebar\" role=\"navigation\" style=\"padding-top: 20px\">";
+        echo "<div class=\"col-md-2\" id=\"sidebar\" role=\"navigation\" style=\"padding-top: 20px\">";
         if ($_SESSION['user_type'] == 'admin') {
             include('/var/www/marking/Views/layouts/sidepanels/adminpanel.view.php');
         } else {
