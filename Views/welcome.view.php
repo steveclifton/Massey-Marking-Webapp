7e1ed334021f1be5898ade4b899061a9cb4c55bf
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-09 08:54:55 +1300

    Fix styling of bootstrap

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -1,26 +1,24 @@
 <h2>Welcome <?= $viewData['first_name'] ?></h2>
 <h4>Current Assignment results</h4>
-<div class="table-responsive">
-    <div class="container">
-        <table class="table table-bordered table-hover">
-            <thead>
-                <tr>
-                    <th>Assignment</th>
-                    <th>Mark (out of 10)</th>
-                    <th>Feedback</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $i = 0;
-                    foreach ($viewData['marks'] as $data) { ?>
-                        <tr>
-                            <td> <?= $data['assignment_number'] ?> </td>
-                            <td> <?= $data['mark'] ?> </td>
-                            <td> None Yet </td>
-                        </tr>
-                    <?php } ?>
-            </tbody>
-        </table>
-    </div>
+<hr><br>
+<div class="table">
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th style="text-align: center">Assignment</th>
+                <th style="text-align: center">Mark</th>
+                <th>Feedback</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($viewData['marks'] as $data) { ?>
+            <tr>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['assignment_number'] ?> </td>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['mark'] ?>/10</td>
+                <td class="col-md-4 col-sm-2"> No feedback yet - User a list item here </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
 </div>
