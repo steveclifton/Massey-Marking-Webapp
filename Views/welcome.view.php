486a752f7099b1f036abd21fef9ae8dcaa03a876
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-08 17:40:52 +1300

    Display a students current results

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -1,5 +1,26 @@
-<div id="centerdisplay" >
-
-    <h1>Student Stuff</h1>
-
+<h2>Welcome <?= $viewData['first_name'] ?></h2>
+<h4>Current Assignment results</h4>
+<div class="table-responsive">
+    <div class="container">
+        <table class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Assignment</th>
+                    <th>Mark (out of 10)</th>
+                    <th>Feedback</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $i = 0;
+                    foreach ($viewData['marks'] as $data) { ?>
+                        <tr>
+                            <td> <?= $data['assignment_number'] ?> </td>
+                            <td> <?= $data['mark'] ?> </td>
+                            <td> None Yet </td>
+                        </tr>
+                    <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
