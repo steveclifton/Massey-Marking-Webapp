828a4ba1348a71f5a719b70aa02f1e23b8a62150
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-09 10:39:19 +1300

    Add method to set feedback

--- Models/Feedback.Model.php
+++ Models/Feedback.Model.php
@@ -12,6 +12,30 @@ use Marking\Exceptions\CustomException;
 class Feedback extends Base
 {
 
+    public function setFeedback($studentId, $semester, $assignment, $feedback)
+    {
+        $result = $this->getAssignmentFeedback($studentId, $assignment, $semester);
+
+        /**
+         * Feedback already exists for this assignment
+         * - Update it
+         */
+        if (isset($result[0])) {
+            $this->updateFeedback($feedback, $result[0]['id']);
+            return;
+            die();
+        }
+
+        /**
+         * Feedback does not exist for this assignment
+         * - Create it
+         */
+        else {
+            $this->writeFeedback($studentId, $semester, $assignment, $feedback);
+        }
+    }
+
+
 
 
 
