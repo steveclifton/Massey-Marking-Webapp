26175d42a2b92ea3f73272f6802cca6f3507e400
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-19 16:47:13 +1300

    Include output in <pre> tags

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -16,7 +16,7 @@
             <tr>
                 <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['assignment_number'] ?> </td>
                 <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['mark'] ?>/10</td>
-                <td class="col-md-4 col-sm-2"> <?= $data ['feedback'] ?> </td>
+                <td class="col-md-4 col-sm-2"> <pre><?= $data ['feedback'] ?></pre> </td>
             </tr>
         <?php } ?>
         </tbody>
