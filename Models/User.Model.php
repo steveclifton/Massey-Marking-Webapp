4eacbefcc04a080e43747ef4c6443cb2dcdbdd91
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-28 15:54:25 +1300

    Refactor method to allow for semester

--- Models/User.Model.php
+++ Models/User.Model.php
@@ -92,15 +92,16 @@ class User extends Base
         return true;
     }
 
-    public function showAllUsers()
+    public function getCurrentSemestersUsers($semester)
     {
-        $sql = "SELECT first_name, last_name, student_id 
+        $sql = "SELECT id, first_name, last_name, student_id, semester
                 FROM `users`
+                WHERE `users`.`semester` = '$semester'
                 ";
 
         $stm = $this->database->prepare(($sql), array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 
-        $stm->execute();
+        $stm->execute(array('$semester'));
 
         $data = $stm->fetchAll(PDO::FETCH_ASSOC);
 
