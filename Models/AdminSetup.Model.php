179894d9a2758b8d06eaed873121805c02691682
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-28 22:02:16 +1300

    Add method to return current semester

--- Models/AdminSetup.Model.php
+++ Models/AdminSetup.Model.php
@@ -12,6 +12,25 @@ use PDO;
  */
 class AdminSetup extends Base
 {
-    
+    public function getCurrentSemester()
+    {
+        $sql = "SELECT semester 
+                FROM `admin_setup` 
+                ORDER BY created_at DESC 
+                LIMIT 1
+                ";
+
+        $stm = $this->database->prepare(($sql), array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+        $stm->execute();
+
+        $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+        if (isset($data[0])) {
+            return $data[0];
+        }
+        header('location: /welcome');
+        die();
+    }
 
 }
