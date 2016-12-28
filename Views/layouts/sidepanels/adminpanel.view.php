d2600f8376e16313bc22104e390f023802e155fc
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-28 16:10:57 +1300

    Refactor Routing

--- Views/layouts/sidepanels/adminpanel.view.php
+++ Views/layouts/sidepanels/adminpanel.view.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['id'])) {
           ";
 
     echo "<li><a href=\"/adduser\" style='text-decoration: underline; font-size: 12pt;'>Single Add User</a></li>";
-    echo "<li><a href=\"/showstudents\" style='text-decoration: underline; font-size: 12pt;'>Show All Students</a></li>";
+    echo "<li><a href=\"/showcurrentstudents\" style='text-decoration: underline; font-size: 12pt;'>Current Semester Students</a></li>";
     echo "<li><a href=\"/testtools\" style='text-decoration: underline; font-size: 12pt;'>Test Tools</a></li>";
 
 }
