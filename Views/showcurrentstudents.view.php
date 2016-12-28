dcfaaf2e07969b00abb54ac61373e2bfdc384911
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-28 16:10:47 +1300

    Refactor naming

--- Views/showcurrentstudents.view.php
+++ Views/showcurrentstudents.view.php
@@ -1,9 +1,12 @@
+<h1 style="text-align: center">Current Semesters Students and Results</h1>
+<hr>
+
+
 <div class="table">
     <table class="table table-bordered table-hover">
         <thead>
             <tr>
                 <th style="text-align: center">Semester</th>
-                <th style="text-align: center">DB ID</th>
                 <th style="text-align: center">Student ID</th>
                 <th style="text-align: center">First Name</th>
                 <th style="text-align: center">Last Name</th>
@@ -21,7 +24,6 @@
         foreach ($viewData['data'] as $data) { ?>
             <tr>
                 <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['semester'] ?></td>
-                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['id'] ?></td>
                 <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['student_id'] ?></td>
                 <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['first_name'] ?></td>
                 <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['last_name'] ?></td>
