e4cfa3e51e5b99aeda6d455de3350ec1348fbb61
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-31 18:43:20 +1300

    Add methods to return the configs

--- Models/AdminSetup.Model.php
+++ Models/AdminSetup.Model.php
@@ -26,11 +26,54 @@ class AdminSetup extends Base
 
         $data = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-        if (isset($data[0])) {
-            return $data[0];
+        if (isset($data[0]['semester'])) {
+            return $data[0]['semester'];
         }
         header('location: /welcome');
         die();
     }
+
+
+    public function getNumberOfAssignments()
+    {
+        $sql = "SELECT num_assignments 
+                FROM `admin_setup` 
+                ORDER BY created_at DESC 
+                LIMIT 1
+                ";
+
+        $stm = $this->database->prepare(($sql), array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+        $stm->execute();
+
+        $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+        if (isset($data[0]['num_assignments'])) {
+            return $data[0]['num_assignments'];
+        }
+        header('location: /welcome');
+        die();
+    }
+
+    public function getNumberOfTests()
+    {
+        $sql = "SELECT num_tests 
+                FROM `admin_setup` 
+                ORDER BY created_at DESC 
+                LIMIT 1
+                ";
+
+        $stm = $this->database->prepare(($sql), array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+        $stm->execute();
+
+        $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+        if (isset($data[0]['num_tests'])) {
+            return $data[0]['num_tests'];
+        }
+        header('location: /welcome');
+        die();
+    }
+
 
 }
