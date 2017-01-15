9f5dad41a9f26aa0e027c8ba06efed90883b6839
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-15 15:51:47 +1300

    Add form to update a students id

--- Views/editstudentprofile.view.php
+++ Views/editstudentprofile.view.php
@@ -1,3 +1,28 @@
-<h3>
-    edit the students profile here
-</h3>
+<h1 style="text-align: center">Current Semesters Students</h1>
+<hr>
+
+<?php if (isset($viewData['students'])) { ?>
+<div class="container">
+    <div class="table">
+        <table class="table table-bordered table-hover">
+            <thead>
+            <tr>
+                <th style="text-align: center">Student ID</th>
+                <th style="text-align: center">First Name</th>
+                <th style="text-align: center">Last Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($viewData['students'] as $data) { ?>
+                <tr>
+                    <td class="col-md-1 col-sm-1" style="text-align: center"><a href="/editstudent?id=<?= $data['student_id'] ?>"><?= $data['student_id'] ?></a></td>
+                    <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['first_name'] ?> </td>
+                    <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['last_name'] ?> </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php } ?>
