d66c377b9a6aa6ee7a428e0061269c7a794b2735
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-17 08:36:31 +1300

    Update folder location

--- Views/404.view.php
+++ Views/404.view.php
@@ -1,1 +1,1 @@
-<img src="../Views/dbtroubles.jpg" width="625" height="416" style="display: block; margin-left: auto; margin-right: auto">
+<img src="/Views/dbtroubles.jpg" width="625" height="416" style="display: block; margin-left: auto; margin-right: auto">
