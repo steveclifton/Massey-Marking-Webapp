3e599349bc21bac02db0f4630d1db08b5026ddcd
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-17 17:11:34 +1300

    Remove path

--- Views/layouts/sidepanels/sidepanel.view.php
+++ Views/layouts/sidepanels/sidepanel.view.php
@@ -2,9 +2,9 @@
     if (isset($_SESSION['user_type'])) {
         echo "<div class=\"col-sm-4 col-md-2\" id=\"sidebar\" role=\"navigation\" style=\"padding-top: 20px\">";
         if ($_SESSION['user_type'] == 'admin') {
-            include('/var/www/marking/Views/layouts/sidepanels/adminpanel.view.php');
+            include('Views/layouts/sidepanels/adminpanel.view.php');
         } else {
-            include('/var/www/marking/Views/layouts/sidepanels/studentpanel.view.php');
+            include('Views/layouts/sidepanels/studentpanel.view.php');
         }
         echo "</div>";
     }
