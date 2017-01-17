95d3379d6dde53baac8a37ecb0cd0d699ee978ee
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-17 17:10:13 +1300

    Refactored view

--- Views/assignment.view.php
+++ Views/assignment.view.php
@@ -1,7 +1,7 @@
 <h1>Mark so far : <?= $viewData['mark'] ?>/10</h1>
 <hr>
 
-<?php include ('/var/www/marking/Views/layouts/upload.view.php'); ?>
+<?php include ('Views/layouts/upload.view.php'); ?>
 
 <h1>Feedback</h1>
 <hr>
